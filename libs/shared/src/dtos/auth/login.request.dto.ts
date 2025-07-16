80aad4a3b0a3674243c714042145db4536ae9b47
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2025-07-16 07:19:47 +0700

    feat(dtos): add ApiProperty decorators to login and registration DTOs for improved Swagger documentation

--- libs/shared/src/dtos/auth/login.request.dto.ts
+++ libs/shared/src/dtos/auth/login.request.dto.ts
@@ -1,14 +1,23 @@
 import { User } from '@app/database/entities/user.entity';
 import { PickType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class LoginRequestDto extends PickType(User, [
   'email',
   'password',
 ] as const) {
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'user@example.com',
+  })
   @IsNotEmpty({ message: 'Email is required' })
   email: string;
 
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'password123',
+  })
   @IsNotEmpty({ message: 'Password is required' })
   password: string;
 }
--- libs/shared/src/dtos/auth/register.request.dto.ts
+++ libs/shared/src/dtos/auth/register.request.dto.ts
@@ -1,5 +1,6 @@
 import { User } from '@app/database/entities/user.entity';
 import { PickType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNotEmpty,
@@ -12,14 +13,26 @@ export class RegisterRequestDto extends PickType(User, [
   'email',
   'password',
 ]) {
+  @ApiProperty({
+    description: 'The full name of the user',
+    example: 'John Doe',
+  })
   @IsNotEmpty({ message: 'Full name is required' })
   @IsString({ message: 'Full name must be a string' })
   fullName: string;
 
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'user@example.com',
+  })
   @IsNotEmpty({ message: 'Email is required' })
   @IsEmail({}, { message: 'Email must be a valid email address' })
   email: string;
 
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'Password123@',
+  })
   @IsNotEmpty({ message: 'Password is required' })
   @IsStrongPassword(
     {
