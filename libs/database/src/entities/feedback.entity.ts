c4d8da6a6f63c2259ef24ad0d9172dbd345b4279
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2025-11-06 23:22:52 +0700

    feat: refactor AuthService to use transactions for login and user retrieval, and enhance CourseService with province and district associations

--- libs/database/src/entities/feedback.entity.ts
+++ libs/database/src/entities/feedback.entity.ts
@@ -2,7 +2,6 @@ import { IsInt, IsNotEmpty, IsString, Max, Min } from 'class-validator';
 import {
   Column,
   CreateDateColumn,
-  DeleteDateColumn,
   Entity,
   JoinColumn,
   ManyToOne,
@@ -33,9 +32,6 @@ export class Feedback {
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
-
-  @DeleteDateColumn({ name: 'deleted_at' })
-  deletedAt?: Date;
 
   @ManyToOne(() => User, (user) => user.createdFeedback, { eager: true })
   @JoinColumn({ name: 'created_by' })
