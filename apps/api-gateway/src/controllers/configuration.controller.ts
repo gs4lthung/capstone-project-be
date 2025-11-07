9346713187b8d2ba78b208cc07c3572ab45915a3
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2025-11-07 15:10:03 +0700

    feat: add findAll method to ConfigurationController and enhance ConfigurationService for pagination, sorting, and filtering

--- apps/api-gateway/src/controllers/configuration.controller.ts
+++ apps/api-gateway/src/controllers/configuration.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Param,
@@ -18,10 +19,36 @@ import { CheckRoles } from '@app/shared/decorators/check-roles.decorator';
 import { UserRole } from '@app/shared/enums/user.enum';
 import { AuthGuard } from '../guards/auth.guard';
 import { RoleGuard } from '../guards/role.guard';
+import { PaginationParams } from '@app/shared/decorators/pagination-params.decorator';
+import { Pagination } from '@app/shared/interfaces/pagination.interface';
+import { Sorting } from '@app/shared/interfaces/sorting.interface';
+import { SortingParams } from '@app/shared/decorators/sorting-params.decorator';
+import { Filtering } from '@app/shared/interfaces/filtering.interface';
+import { FilteringParams } from '@app/shared/decorators/filtering-params.decorator';
 
 @Controller('configurations')
 export class ConfigurationController {
   constructor(private readonly configurationService: ConfigurationService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    tags: ['Configurations'],
+    summary: 'Get all configurations',
+    description: 'Retrieve a list of all configurations',
+  })
+  async findAll(
+    @PaginationParams()
+    pagination: Pagination,
+    @SortingParams() sort: Sorting,
+    @FilteringParams() filter: Filtering,
+  ) {
+    return this.configurationService.findAll({
+      pagination,
+      sort,
+      filter,
+    });
+  }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
